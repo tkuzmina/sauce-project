6639236e47787991508350bd11c47e225aab9ebc
tkuzmina <tatjana_kuzmina@inbox.lv>
2014-12-24 14:00:03 +0000

    second change

--- src/test/java/com/saucelabs/SampleSauceTest.java
+++ src/test/java/com/saucelabs/SampleSauceTest.java
@@ -1,6 +1,8 @@
 package com.saucelabs;
 import static org.junit.Assert.*;
+
 import com.saucelabs.common.SauceOnDemandAuthentication;
+
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
@@ -11,10 +13,12 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.remote.CapabilityType;
 import org.openqa.selenium.remote.DesiredCapabilities;
 import org.openqa.selenium.remote.RemoteWebDriver;
+
 import com.saucelabs.junit.Parallelized;
 import com.saucelabs.junit.ConcurrentParameterized;
 import com.saucelabs.junit.SauceOnDemandTestWatcher;
 
+import java.net.MalformedURLException;
 import java.net.URL;
 import java.util.LinkedList;
 
@@ -22,6 +26,7 @@ import static org.junit.Assert.assertEquals;
 
 import com.saucelabs.common.SauceOnDemandAuthentication;
 import com.saucelabs.common.SauceOnDemandSessionIdProvider;
+
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Rule;
@@ -39,9 +44,36 @@ import static org.junit.Assert.assertEquals;
 
 public class SampleSauceTest {
 
+	private WebDriver driver;
+
+    @Before
+    public void setUp() throws Exception {
+        // Choose the browser, version, and platform to test
+        DesiredCapabilities capabilities = DesiredCapabilities.firefox();
+        capabilities.setCapability("version", "5");
+        capabilities.setCapability("platform", Platform.XP);
+        // Create the connection to Sauce Labs to run the tests
+        this.driver = new RemoteWebDriver(
+                new URL("http://tatjanakuzmina:1d59bec5-288e-41d8-814d-bc23db3fafa8@ondemand.saucelabs.com:80/wd/hub"),
+                capabilities);
+    }
+
     @Test
-    public void test() throws Exception {
+    public void webDriver() throws Exception {
+        // Make the browser get the page and check its title
+        driver.get("http://www.amazon.com/");
+        assertEquals("Amazon.com: Online Shopping for Electronics, Apparel, Computers, Books, DVDs & more", driver.getTitle());
+    }
+
+    @After
+    public void tearDown() throws Exception {
+        driver.quit();
+    }
+    
+    /*public void test() throws Exception {
     	System.out.println("Test success");
         assertTrue("Success", true);
     }
+    */
+    
 }
