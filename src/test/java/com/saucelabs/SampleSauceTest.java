7f4fdae4a163137a3cc8613d915ebe20a1d85512
tkuzmina <tatjana_kuzmina@inbox.lv>
2014-12-24 11:54:26 +0000

    first modifications

---
+++ src/test/java/com/saucelabs/SampleSauceTest.java
@@ -0,0 +1,47 @@
+package com.saucelabs;
+import static org.junit.Assert.*;
+import com.saucelabs.common.SauceOnDemandAuthentication;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
+import org.openqa.selenium.Platform;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.remote.CapabilityType;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.remote.RemoteWebDriver;
+import com.saucelabs.junit.Parallelized;
+import com.saucelabs.junit.ConcurrentParameterized;
+import com.saucelabs.junit.SauceOnDemandTestWatcher;
+
+import java.net.URL;
+import java.util.LinkedList;
+
+import static org.junit.Assert.assertEquals;
+
+import com.saucelabs.common.SauceOnDemandAuthentication;
+import com.saucelabs.common.SauceOnDemandSessionIdProvider;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.remote.CapabilityType;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.remote.RemoteWebDriver;
+
+import java.net.URL;
+import java.util.LinkedList;
+
+import static org.junit.Assert.assertEquals;
+
+public class SampleSauceTest {
+
+    @Test
+    public void test() throws Exception {
+    	System.out.println("Test success");
+        assertTrue("Success", true);
+    }
+}
